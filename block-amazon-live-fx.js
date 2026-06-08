064393f7dd01285af26d0e68707a570db04208e2
Ramit Mukherjee <ramit.mkj6@gmail.com>
2026-06-08 09:29:14 +0530

    adding icon

--- block-amazon-live-fx.js
+++ block-amazon-live-fx.js
@@ -1,3 +1,3 @@
 //document.body.style.border = "5px solid red";
-const amazonLiveDiv = document.querySelector('*[data-csa-c-painter*="AmazonLive"]');
+const amazonLiveDiv = document.querySelector('*[data-csa-c-painter*="AmazonLive" i]');
 amazonLiveDiv.remove();
